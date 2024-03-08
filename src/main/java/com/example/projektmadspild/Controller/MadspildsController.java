fd88195dd2b031442ff04409d888cddd3742c15f
bjli0001 <bjli0001@stud.kea.dk>
2024-03-08 11:56:01 +0100

    oprettet forside

--- src/main/java/com/example/projektmadspild/Controller/MadspildsController.java
+++ src/main/java/com/example/projektmadspild/Controller/MadspildsController.java
@@ -1,4 +1,14 @@
 package com.example.projektmadspild.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
 public class MadspildsController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
 }
