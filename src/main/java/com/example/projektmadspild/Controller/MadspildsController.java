d41809d9e50727f26c0b17af3e976a6a66793e45
YoussefEL-M <yoel0001@stud.kea.dk>
2024-03-11 11:51:02 +0100

    Tilføjet tips og tricks side, og tilføejet en redirect til siden i controlleren.

--- src/main/java/com/example/projektmadspild/Controller/MadspildsController.java
+++ src/main/java/com/example/projektmadspild/Controller/MadspildsController.java
@@ -2,6 +2,8 @@ package com.example.projektmadspild.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -10,5 +12,21 @@ public class MadspildsController {
     public String index(){
         return "index";
     }
+    @PostMapping("/redirect")
+    public String redirect(RedirectAttributes attributes){
+
+        attributes.addAttribute("redirectUrl", "/tipsogtricks");
+        return "redirect:/tips";
+    }
+    @GetMapping("/redirecting")
+    public String redirectToTipsogtricks() {
+        return "redirect:/tipsogtricks";
+    }
+
+    @GetMapping("/tipsogtricks")
+    public String tipsSide() {
+        return "tipsOgTricks";
+    }
+
 
 }
